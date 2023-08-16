d9e69abefe90aaaed94348ba109c4d80b2cb028e
mianumairkhalid <computeexpert@gmail.com>
2023-08-16 15:29:24 +0500

    Update CollectPoints.jsx

--- src/pages/CollectPoints.jsx
+++ src/pages/CollectPoints.jsx
@@ -6,7 +6,7 @@ function CollectPoints() {
   return (
     <>
     <TitleHeader link={'/my-account'} title={'วิธีเก็บคะแนน'} />
-    <div className="px-[20px] pt-9 mb-[120px]">
+    <div className="px-[20px] pt-9 mt-10">
         <h2 className='text-base font-bold' style={{ fontFamily: "Eventpop" }}>เงื่อนไขในการให้คะแนน </h2>
         <ul className='mt-2 ml-5'>
             <li className='text-sm font-medium text-[#585858] list-disc leading-[20px]'>ค่าบริการปกติ ทุก 500 บาท มีค่าเท่ากับ 1 คะแนน </li>
