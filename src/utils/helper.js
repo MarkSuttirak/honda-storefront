4bc7ece954157a6eb3240a4cc8b90db3ee52b087
Nabeel Goraya <itsgoraya@gmail.com>
2023-08-16 15:05:00 +0500

    update

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,11 +1,10 @@
 import Cookies from 'js-cookie';
+import { ReactSession } from 'react-client-session';
+ReactSession.setStoreType("sessionstorage");
+
 
 const TokenKey = 'token';
-
-const getToken = () => Cookies.get(TokenKey);
-
-const setToken = (token) => Cookies.set(TokenKey, token);
-
-const removeToken = () => Cookies.remove(TokenKey);
-
+const getToken = () => ReactSession.get(TokenKey);
+const setToken = (token) => ReactSession.set(TokenKey, token);
+const removeToken = () => sessionStorage.removeItem(TokenKey);
 export { getToken, removeToken, setToken };
