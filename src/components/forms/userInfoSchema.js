b8a039fbbfe364e342a91b4c3e296b5ec446c65f
MarkSuttirak <suttirak.ch@zaviago.com>
2024-06-05 09:58:20 +0700

    Modify the required inputs

--- src/components/forms/userInfoSchema.js
+++ src/components/forms/userInfoSchema.js
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 export const userInfoSchema = Yup.object().shape({
     first_name: Yup.string().required('First name is required'),
     last_name: Yup.string().required('Last name is required'),
-    // email: Yup.string().email('Invalid email').required('Email is required'),
+    email: Yup.string().email('Invalid email').required('Email address is required'),
     id_card_number: Yup.string().required('ID card number is required'),
     birth_date: Yup.date().required('Birth date is required'),
     // phone: Yup.string().required('Phone is required'),
