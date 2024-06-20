dd6a995f8c4eb728c7492ed95d90ab5ce77e3ee4
Nabeel Goraya <itsgoraya@gmail.com>
2024-06-20 16:55:28 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -68,14 +68,14 @@ export default function App() {
   
   useEffect(() => {
     if (isPhoneVerified) {
-      navigate("/phoneverify");
+      navigate("/phonverify");
     }
     if (token) {
       
       Cookies.set('username', username);
       if (phoneverify == 'true') {
         Cookies.set('phoneverify', true);
-        navigate("/phoneverify");
+        navigate("/phonverify");
       }
       else {
         navigate("/login");
